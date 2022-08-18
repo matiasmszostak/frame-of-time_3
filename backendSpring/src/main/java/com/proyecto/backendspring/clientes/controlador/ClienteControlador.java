3cd62c0fce0266681099afdc3146ba5f6815a1cb
Matias Szostak <mszostak@petersen.corp>
2022-08-18 00:45:40 -0300

    Arreglos en Template Driven Form de Contacto

--- backendSpring/src/main/java/com/proyecto/backendspring/clientes/controlador/ClienteControlador.java
+++ backendSpring/src/main/java/com/proyecto/backendspring/clientes/controlador/ClienteControlador.java
@@ -4,6 +4,7 @@ import com.proyecto.backendspring.clientes.models.Cliente;
 import com.proyecto.backendspring.clientes.repositorio.ClienteRepositorio;
 import com.proyecto.backendspring.clientes.services.ClienteDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,16 +41,28 @@ public class ClienteControlador {
 
     //guardar un cliente
     @PostMapping()
-    public Cliente guardarCliente(@RequestBody Cliente cliente){
-        return repositorio.save(cliente);
+    public ResponseEntity<?> guardarCliente(@RequestBody Cliente cliente){
+        Cliente clienteReturn = null; //inicializo variable en null
 
+        //Try Catch, para atajar el error que definí en el servicio. Si el cliente ya existe, devuelve el mensaje de error
+        try{
+            clienteReturn = this.servicio.altaNuevoCliente(cliente); // si sale bien, se guarda en la variable
+
+        }catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+        //Si no hay error, devuelve el cliente con un status de cliente creado
+        return new ResponseEntity<>(clienteReturn, HttpStatus.CREATED);
     }
 
 
 
 
 
-    
+
+
 
 
 
--- frontendAngular/src/app/componentes/formularios/formularioContacto/formularioContacto.component.ts
+++ frontendAngular/src/app/componentes/formularios/formularioContacto/formularioContacto.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Cliente } from 'src/app/cliente';
 import { ClienteService } from '../../../cliente.service';
-import { FormGroup, FormBuilder, FormControl, Validators  } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,12 +19,12 @@ export class FormularioContactoComponent implements OnInit {
 
   myForm: FormGroup;
 
-  cliente : Cliente = new Cliente();
+  cliente: Cliente = new Cliente();
 
 
   constructor(
-    private clienteServicio:ClienteService,
-    private router:Router) { }
+    private clienteServicio: ClienteService,
+    private router: Router) { }
 
 
 
@@ -42,27 +42,32 @@ export class FormularioContactoComponent implements OnInit {
   }
 
 
-  guardarCliente(){
+  guardarCliente() {
     this.clienteServicio.registrarCliente(this.cliente).subscribe(dato => {
       console.log(dato);
-  //    this.irALaListaDeClientes();   va a la lista de clientes, no lo voy a usar
+      //    this.irALaListaDeClientes();   va a la lista de clientes, no lo voy a usar
     });
   }
 
-  irALaListaDeClientes(){
+  irALaListaDeClientes() {
     this.router.navigate(['/clientes'])
   }
 
 
-  enviado=false;
+  enviado = false;
 
-  onSubmit(){
+  onSubmit() {
+
+
     this.guardarCliente();
     console.log(this.cliente);
     this.cliente = new Cliente();
     this.myForm.reset();
+
+    /*    this.myForm.markAsPristine();
+        this.myForm.markAsUntouched();
+            */
+    this.myForm.updateValueAndValidity();
   }
-
-
 
 }
